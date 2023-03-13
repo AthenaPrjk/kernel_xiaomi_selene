b71f090d56d336b274d72a1961211ae30bf497b6
Renayura <renayura@proton.me>
2023-03-13 20:45:39 +0000

    sound/: Avoid collision with clang's `BIT` macro
    ../sound/soc/mediatek/fs1815n/fsm-dev.h:231:9: error: 'BIT' macro redefined [-Werror,-Wmacro-redefined]
            ^
    ../include/linux/bits.h:8:9: note: previous definition is here
            ^
    
    Signed-off-by: Renayura <renayura@proton.me>

--- sound/soc/mediatek/fs1815n/fsm-dev.h
+++ sound/soc/mediatek/fs1815n/fsm-dev.h
@@ -228,6 +228,7 @@ enum dev_id_index {
 #define FSM_DEV_MAX   (4)
 #define FSM_ADDR_BASE (0x34)
 #define MIN(a, b)     ((a) < (b) ? (a) : (b))
+#undef BIT
 #define BIT(nr)       (1UL << (nr))
 #define HIGH8(val)    ((val >> 8) & 0xFF)
 #define LOW8(val)     (val & 0xFF)
